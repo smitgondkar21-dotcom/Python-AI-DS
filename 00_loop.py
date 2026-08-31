35d6964ac240370a89dbe0a16eae775d3fc309ca
smitgondkar21-dotcom <smitgondkar21@gmail.com>
2026-08-31 11:04:49 +0530

    Add files via upload

---
+++ 00_loop.py
@@ -0,0 +1,8 @@
+name = input("give me ur name")
+num = int(input("no's of times you want to print"))
+
+for i in range(num):
+
+    print(name)
+
+print(i)
---
+++ ascii_art.py
@@ -0,0 +1,8 @@
+import pyfiglet
+
+text = input("Enter text: ")
+
+ascii_art = pyfiglet.figlet_format(text)
+
+
+print(ascii_art)
